2252be8e4865f11284b7eb357cc002f123762314
suhitd1729 <32601049+suhitd1729@users.noreply.github.com>
2019-01-21 13:45:00 -0500

    adding callback and error handling

--- node_examples/index.js
+++ node_examples/index.js
@@ -1,22 +1,25 @@
-var rect = {
-	perimeter : (x,y) => (2*(x+y)),
-	area : (x,y) => (x*y)
+var rect = require('./rectangle');
+
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
 };
 
-function solveRect(l,b){
-	console.log("Solving for rectangle with l = " + l + " and b = " + b);
-
-	if (l <= 0 || b <= 0){
-		console.log("Rectangle dimensions should be greater than 0");
-	}
-	else{
-		console.log("The area is " +rect.area(l,b));
-		console.log("The perimeter is " +rect.perimeter(l,b));
-
-	}
-}
 
 solveRect(2,4);
 solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+solveRect(-1,9);
+solveRect(4,-7);
+solveRect(-1,-2);
